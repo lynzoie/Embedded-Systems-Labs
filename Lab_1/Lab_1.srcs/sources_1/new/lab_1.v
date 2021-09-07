d177ff7760c175866fa55bceddfd0832b7f409c7
lynzoie <69117554+lynzoie@users.noreply.github.com>
2021-09-07 17:59:33 -0400

    adding constraints and sources

---
+++ Lab_1/Lab_1.srcs/sources_1/new/lab_1.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09/07/2021 05:54:53 PM
+// Design Name: 
+// Module Name: lab_1
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module lab_1(
+
+    );
+endmodule
