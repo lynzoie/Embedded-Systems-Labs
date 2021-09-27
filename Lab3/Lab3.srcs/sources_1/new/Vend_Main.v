bf2ee5b1cef6347dec6c4143dea2c9239fd0541d
Andi Thomas <andilt5@outlook.com>
2021-09-27 16:32:57 -0400

    Anna and Will Updates 0927

--- Lab3/Lab3.srcs/sources_1/new/Vend_Main.v
+++ Lab3/Lab3.srcs/sources_1/new/Vend_Main.v
@@ -3,17 +3,17 @@
 
 module Vend_Main(
     input Clk,
-    input [3:0] money_in,           //dollars, quarters, and bearcat card
-    input [7:0] product_in,         //product selection
-    input dispense,                 //switch to enable dispense
+    input [3:0] money_in, //dollars, quarters, and bearcat card
+    input [7:0] product_in, //product selection
+    input dispense, //switch to enable dispense
 
 
-    output [7:0] product_out,       //product dispensed
-    output [1:0] changeLED,         //change dispensed
-    output [1:0] alert,             //not enough money or out of stock
-    output [3:0] Anode_Activate,    //turn 7-seg on
-    output [6:0] LED_out,           //display digit on 7-seg
-    output decimal                  //display decimal on 7-seg
+    output [7:0] product_out, //product dispensed
+    output [1:0] changeLED, //change dispensed
+    output [1:0] alert, //not enough money or out of stock
+    output [3:0] Anode_Activate, //turn 7-seg on
+    output [6:0] LED_out, //display digit on 7-seg
+    output decimal //display decimal on 7-seg
 
 );
     //track money
@@ -53,125 +53,126 @@ module Vend_Main(
     reg change_state = 0;
     
     ////////////////////////////Calculate Input Money//////////////////
-    always@(posedge (money_in != 4'b0000))              //when dollars, quarters, bearcat card are entered
+always@(posedge (money_in != 4'b0000))              //when dollars, quarters, bearcat card are entered
     begin
-    
-        seg7_count = 0; //for button debouncing
 
-        if(money_in == 4'b0001) begin                   //if quarter button pressed
-            quartersValue = quartersValue + 1;          //add 1 to quarterValue
-            if (quartersValue == 4) begin               //if 4 quarters entered
-                dollarsValue <= dollarsValue + 1;       //convert to dollar
-                quartersValue <= 0;                     //reset quarters
-            end
-        end
-        else if(money_in == 4'b0010) begin              //if dollar entered
-            dollarsValue = dollarsValue + 1;            //add 1 to dollarValue
-        end
-        else if(money_in == 4'b0100) begin              //reset aka give change
-            dollarsValue = 0;
-            quartersValue = 0;
-        end
-        else if(money_in == 4'b1000) begin              //bearcat card entered (needs work)
-            dollarsValue = 0;
-            quartersValue = 0;
-        end
+    //seg7_count = 0; //for button debouncing
 
-        while (seg7_count < 500) begin                  //for button debouncing
-            seg7_count = seg7_count + 1;
+    if(money_in == 4'b0001) begin                   //if quarter button pressed
+        quartersValue = quartersValue + 1;          //add 1 to quarterValue
+        if (quartersValue == 4) begin               //if 4 quarters entered
+            dollarsValue = dollarsValue + 1;        //convert to dollar
+            quartersValue = 0;                      //reset quarters
         end
-
     end
+    else if(money_in == 4'b0010) begin              //if dollar entered
+        dollarsValue = dollarsValue + 1;            //add 1 to dollarValue
+    end
+    else if(money_in == 4'b0100) begin              //reset aka give change
+        dollarsValue = 0;
+        quartersValue = 0;
+    end
+    else if(money_in == 4'b1000) begin              //bearcat card entered (needs work)
+        dollarsValue = 0;
+        quartersValue = 0;
+    end
+
+    /* while (seg7_count < 500) begin                  //for button debouncing
+            seg7_count = seg7_count + 1;
+        end */
+
+end
     //end
 
-    //////////////////Set Money Display Variable/////////////////
+    //////////////////Calculate Spent Money/////////////////
+always@((dollarsSpent) or (quartersSpent) or (dollarsValue) or (quartersValue)) begin
+    //If money is either entered or spent
+    dollarsCurrent = dollarsValue; //move money input into current
+    quartersCurrent = quartersValue; //move mpmey input into current
 
-    //calculate money
-    always@(posedge Clk) begin
-        //If money is either entered or spent
-        dollarsCurrent = dollarsValue;          //move money input into current
-        quartersCurrent = quartersValue;        //move mpmey input into current
-
-        if((quartersSpent != 0) || (dollarsSpent != 0)) begin       //if money was spent
-            if (quartersCurrent < quartersSpent) begin              //not enough quarters
-                dollarsCurrent <= dollarsCurrent - 1;               //change for a dollar
-                quartersCurrent <= quartersCurrent + 4;             //new change
-                quartersCurrent <= quartersCurrent - quartersSpent; //new amount
-            end
-            if (quartersCurrent >= quartersSpent) begin             //if you entered enough quarters
-                quartersCurrent <= quartersCurrent - quartersSpent; //subtract quarters spent
-            end
-
-            if (dollarsCurrent >= dollarsSpent) begin               //if enough dollars entered
-                dollarsCurrent <= dollarsCurrent - dollarsSpent;    //subrtact quarters spent
-            end
+    if((quartersSpent != 0) || (dollarsSpent != 0)) begin //if money was spent
+        if (quartersCurrent < quartersSpent) begin //not enough quarters
+            dollarsCurrent <= dollarsCurrent - 1; //change for a dollar
+            quartersCurrent <= quartersCurrent + 4; //new change
+            quartersCurrent <= quartersCurrent - quartersSpent; //new amount
         end
-        
-        //set dollar display
-        if(dollarsCurrent < 10) begin                                       //displays 0#.##
-            foursDigit = 0;
-            threesDigit = dollarsValue;
+        if (quartersCurrent >= quartersSpent) begin //if you entered enough quarters
+            quartersCurrent <= quartersCurrent - quartersSpent; //subtract quarters spent
         end
-        else if((dollarsCurrent >= 10) && (dollarsCurrent < 20)) begin     //displays 1#.##
-            foursDigit = 1;
-            threesDigit = dollarsCurrent - 10;
+        if (dollarsCurrent >= dollarsSpent) begin //if enough dollars entered
+            dollarsCurrent <= dollarsCurrent - dollarsSpent; //subrtact quarters spent
         end
-        else if((dollarsCurrent >= 20) && (dollarsCurrent < 30)) begin     //displays 2#.##
-            foursDigit = 2;
-            threesDigit = dollarsCurrent - 20;
-        end
-        else if((dollarsCurrent >= 30) && (dollarsCurrent < 40)) begin     //displays 3#.##
-            foursDigit = 3;
-            threesDigit = dollarsCurrent - 30;
-        end
-        else if((dollarsCurrent >= 40) && (dollarsCurrent < 50)) begin     //displays 4#.##
-            foursDigit = 4;
-            threesDigit = dollarsCurrent - 40;
-        end
-        else if((dollarsCurrent >= 50) && (dollarsCurrent < 60)) begin     //displays 5#.##
-            foursDigit = 5;
-            threesDigit = dollarsCurrent - 50;
-        end
-        else if((dollarsCurrent >= 60) && (dollarsCurrent < 70)) begin     //displays 6#.##
-            foursDigit = 6;
-            threesDigit = dollarsCurrent - 60;
-        end
-        else if((dollarsCurrent >= 70) && (dollarsCurrent < 80)) begin     //displays 7#.##
-            foursDigit = 7;
-            threesDigit = dollarsCurrent - 70;
-        end
-        else if((dollarsCurrent >= 80) && (dollarsCurrent < 90)) begin     //displays 8#.##
-            foursDigit = 8;
-            threesDigit = dollarsCurrent - 80;
-        end
-        else if((dollarsCurrent >= 90) && (dollarsCurrent < 100)) begin     //displays 9#.##
-            foursDigit = 9;
-            threesDigit = dollarsCurrent - 90;
-        end
-        //set quarter display
-        case(quartersCurrent)
-            0: begin //0 cents
-                onesDigit = 0;
-                twosDigit = 0; end
-            1: begin //25 cents
-                onesDigit = 5;
-                twosDigit = 2; end
-            2: begin //50 cents
-                onesDigit = 0;
-                twosDigit = 5; end
-            3: begin //75 cents
-                onesDigit = 5;
-                twosDigit = 7; end
-            default: begin //0 cents
-                onesDigit = 0;
-                twosDigit = 0; end
-        endcase
     end
+end
 
+    //////////////////Set Dollar Display Variables/////////////////
+always @((dollarsCurrent) or (dollarsValue)) begin
+    if(dollarsCurrent < 10) begin //displays 0#.##
+        foursDigit = 0;
+        threesDigit = dollarsCurrent;
+    end
+    else if((dollarsCurrent >= 10) && (dollarsCurrent < 20)) begin //displays 1#.##
+        foursDigit = 1;
+        threesDigit = dollarsCurrent - 10;
+    end
+    else if((dollarsCurrent >= 20) && (dollarsCurrent < 30)) begin //displays 2#.##
+        foursDigit = 2;
+        threesDigit = dollarsCurrent - 20;
+    end
+    else if((dollarsCurrent >= 30) && (dollarsCurrent < 40)) begin //displays 3#.##
+        foursDigit = 3;
+        threesDigit = dollarsCurrent - 30;
+    end
+    else if((dollarsCurrent >= 40) && (dollarsCurrent < 50)) begin //displays 4#.##
+        foursDigit = 4;
+        threesDigit = dollarsCurrent - 40;
+    end
+    else if((dollarsCurrent >= 50) && (dollarsCurrent < 60)) begin //displays 5#.##
+        foursDigit = 5;
+        threesDigit = dollarsCurrent - 50;
+    end
+    else if((dollarsCurrent >= 60) && (dollarsCurrent < 70)) begin //displays 6#.##
+        foursDigit = 6;
+        threesDigit = dollarsCurrent - 60;
+    end
+    else if((dollarsCurrent >= 70) && (dollarsCurrent < 80)) begin //displays 7#.##
+        foursDigit = 7;
+        threesDigit = dollarsCurrent - 70;
+    end
+    else if((dollarsCurrent >= 80) && (dollarsCurrent < 90)) begin //displays 8#.##
+        foursDigit = 8;
+        threesDigit = dollarsCurrent - 80;
+    end
+    else if((dollarsCurrent >= 90) && (dollarsCurrent < 100)) begin //displays 9#.##
+        foursDigit = 9;
+        threesDigit = dollarsCurrent - 90;
+    end
+end
+        
+    //////////////////Set Quarters Display Variables/////////////////
+always @((quartersCurrent) or (quartersValue)) begin
+    case(quartersCurrent)
+        0: begin //0 cents
+            onesDigit = 0;
+            twosDigit = 0; end
+        1: begin //25 cents
+            onesDigit = 5;
+            twosDigit = 2; end
+        2: begin //50 cents
+            onesDigit = 0;
+            twosDigit = 5; end
+        3: begin //75 cents
+            onesDigit = 5;
+            twosDigit = 7; end
+        default: begin //0 cents
+            onesDigit = 0;
+            twosDigit = 0; end
+    endcase
+end
 
-    ////////////////////////////Select Lit 7-Seg//////////////////
+////////////////////////////Select Lit 7-Seg//////////////////
 
-    always @(posedge Clk) begin //Activate one of four 7-seg displays
+always @(posedge Clk) begin //Activate one of four 7-seg displays
         refresh_counter = refresh_counter + 1;      //increment counter
         if(refresh_counter == 5000)                 //at 500
             LED_activating_counter = 0;             //light onesDigit
@@ -185,9 +186,9 @@ module Vend_Main(
             refresh_counter = 0;                    //start over at 0
     end
 
-    ////////////////////////////Set Displayed Digit/////////////
+////////////////////////////Set Displayed Digit/////////////
 
-    always @(LED_activating_counter)                    //when 7-seg digit changes
+always @(LED_activating_counter)                    //when 7-seg digit changes
     begin
         //LED_activating_counter = refresh_counter;
         case(LED_activating_counter)                    //activate the digit
@@ -214,9 +215,9 @@ module Vend_Main(
         endcase
     end
 
-    ////////////////////////////Display Digit//////////////////
+////////////////////////////Display Digit//////////////////
 
-    always @(LED_BCD)
+always @(LED_BCD)
     begin
         case(LED_BCD)
             4'b0000: LED_out_Var = 7'b0000001; // "0"     
@@ -233,15 +234,15 @@ module Vend_Main(
         endcase
     end
 
-    ///////////////////////////Output Display///////////////////////////
-    assign Anode_Activate = Anode_Activate_Var;
-    assign LED_out = LED_out_Var;
-    assign decimal = decimalValue;
+///////////////////////////Output Display///////////////////////////
+assign Anode_Activate = Anode_Activate_Var;
+assign LED_out = LED_out_Var;
+assign decimal = decimalValue;
 
-    //////////////////Monitors Current State and Debounce//////////////
-    always@(posedge Clk) begin              //at clock (synched with state machine)
+//////////////////Monitors Current State and Debounce//////////////
+always@(posedge Clk) begin                  //at clock (synched with state machine)
         cstate = dispense;                  //current state is switch input
-        if(cstate == 0) begin
+        /*if(cstate == 0) begin
             state = 0;                      //restart
             nstate = 0;                     //restart
         end
@@ -251,18 +252,18 @@ module Vend_Main(
         else begin                          //if they are not the same
             change_state = 1;               //do not change states
         end
-        pstate = cstate;                    //past state is current state
+        pstate = cstate;*/                    //past state is current state
     end
-    
-    /////////////////////////Dispense Product////////////////////////
-    always @(posedge Clk) begin
+
+/////////////////////////Dispense Product////////////////////////
+always @(posedge Clk) begin
         productSelect = product_in;         //record product selection
         case(state)                         //move to state
             0: begin                        // State 0: check product price
                 quartersSpent = 0;          //reset money spent
                 dollarsSpent = 0;           //reset money spent
                 
-                //breadcrumb = 2'b01;
+                breadcrumb = 2'b01;
                 
                 if((productSelect == 8'b00000001) || (productSelect == 8'b00000010)) begin
                     nstate = 1;             //$0.50 state
@@ -273,9 +274,10 @@ module Vend_Main(
                 else if((productSelect == 8'b00010000) || (productSelect == 8'b00100000) || (productSelect == 8'b01000000) || (productSelect == 8'b10000000)) begin
                     nstate = 3;             //$1.25 state
                 end
-                else begin nstate = 0;      //stay put
+                else begin
+                    nstate = 0;             //stay put
                 end
-                if(change_state == 1) begin //if ready to switch states
+                if(cstate == 1) begin       //if ready to switch states
                     state = nstate;         //do itttttt :)
                 end
             end
@@ -285,21 +287,20 @@ module Vend_Main(
 
                 if ((quartersValue >= 2) || (dollarsValue >= 1)) begin //$0.50
                     quartersSpent = 2;      //amount we will subtract
-                    nstate = 0;
+                    state = 0;
                     if (productSelect == 8'b00000001)
                         productDispensed = 8'b00000001;
 
                     if (productSelect == 8'b00000010)
                         productDispensed = 8'b00000010;
-                    nstate = 4;
                 end
                 else begin 
-                    nstate = 1;
+                    state = 1;
                     //alert need more money
                 end
-                if(change_state == 1) begin //if ready to switch states
+                /*if(change_state == 1) begin //if ready to switch states
                     state = nstate;         //do itttttt :)
-                end
+                end*/
             end
 
             2: begin // State 2
@@ -337,9 +338,8 @@ module Vend_Main(
         endcase
     end
 
-
-    assign product_out = productDispensed;
-    //assign alert = breadcrumb;
+assign product_out = productDispensed;
+assign alert = breadcrumb;
 
 
 endmodule
